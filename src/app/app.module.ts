3d2c58f4c477fb0b21157fdbba2e2ec03877880a
jihanko42 <jihanko42@gmail.com>
2021-07-29 22:07:39 -0400

    added componenets and modules for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './feature/home/home.component';
+import { AboutComponent } from './feature/about/about.component';
+import { ContactComponent } from './feature/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feature/experience/experience.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExperienceComponent } from './experience.component';
+
+
+
+@NgModule({
+  declarations: [ExperienceComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExperienceModule { }

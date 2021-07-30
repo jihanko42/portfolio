dacb309fc3210b99261001388505d2e11d446fb3
jihanko42 <jihanko42@gmail.com>
2021-07-29 22:51:31 -0400

    first routing to home component wired up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from '@shared/shared.module';
-import { HomeComponent } from '@feature/home/home.component';
-import { AboutComponent } from '@feature/about/about.component';
-import { ContactComponent } from '@feature/contact/contact.component';
 import { CoreModule } from '@core/core.module';
 import { FeatureModule } from '@feature/feature.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutModule } from './about/about.module';
+import { ContactModule } from './contact/contact.module';
+import { ExperienceModule } from './experience/experience.module';
+import { HomeModule } from './home/home.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AboutModule,
+    ContactModule,
+    ExperienceModule,
+    HomeModule
   ]
 })
 export class FeatureModule { }
